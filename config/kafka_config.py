3cf603b1f845e9e05d147da74289d3859e39adef
BartekFirlej <yari12@wp.pl>
2024-05-14 21:02:04 +0200

    Created pipeline

---
+++ config/kafka_config.py
@@ -0,0 +1,4 @@
+# Configuration for Kafka
+KAFKA_BROKER = 'localhost:9092'
+KAFKA_GROUP_ID = 'my-group'
+KAFKA_TOPICS = ['productView']
---
+++ src/kafka_consumer.py
@@ -0,0 +1,38 @@
+from confluent_kafka import Consumer, KafkaException, KafkaError
+from config.kafka_config import KAFKA_BROKER, KAFKA_GROUP_ID, KAFKA_TOPICS
+
+
+def consume_messages():
+    conf = {
+        'bootstrap.servers': KAFKA_BROKER,
+        'group.id': KAFKA_GROUP_ID,
+        'auto.offset.reset': 'earliest'
+    }
+
+    consumer = Consumer(conf)
+
+    def print_assignment(consumer, partitions):
+        print('Assignment:', [p.topic + ":" + str(p.partition) for p in partitions])
+
+    consumer.subscribe(KAFKA_TOPICS, on_assign=print_assignment)
+
+    try:
+        while True:
+            msg = consumer.poll(timeout=1.0)
+            if msg is None:
+                continue
+            if msg.error():
+                if msg.error().code() == KafkaError._PARTITION_EOF:
+                    # End of partition event
+                    print(f'Reached end of partition {msg.partition()} at offset {msg.offset()}')
+                elif msg.error():
+                    raise KafkaException(msg.error())
+            else:
+                # Proper message
+                print(
+                    f'Received message from topic {msg.topic()} partition {msg.partition()} offset {msg.offset()}: key={msg.key()} value={msg.value().decode("utf-8")}')
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # Close down consumer to commit final offsets.
+        consumer.close()
